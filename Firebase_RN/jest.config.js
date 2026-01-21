6964e12c5b46c0d1acc8f6e12962ab4d397c2142
Hardik Trivedi <ihardiktrivedi.ihart@gmail.com>
2026-01-21 12:26:16 +0530

    code coverge generatedd

--- Firebase_RN/jest.config.js
+++ Firebase_RN/jest.config.js
@@ -1,9 +1,22 @@
 module.exports = {
   preset: 'react-native',
+
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|@reduxjs/toolkit|immer)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@reduxjs|firebase)/)',
   ],
+
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/**/index.js'],
+
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}', // ✅ IMPORTANT
+    '!src/**/*.d.ts',
+    '!src/**/styles.ts',
+    '!src/**/types/**',
+    '!src/**/__tests__/**',
+    '!src/**/index.ts',
+  ],
+
+  coverageReporters: ['text', 'lcov'],
 };
